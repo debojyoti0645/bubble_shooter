dab2d76650f6fd33e6a38733e591a31062860d9c
Debojyoti Debnath <debojyoti0645@gmail.com>
2025-06-17 18:38:06 +0530

    feat: Add ad unit IDs as comments in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -21,3 +21,8 @@ class MainApp extends StatelessWidget {
     );
   }
 }
+
+// App Ad Unit ID = ca-app-pub-7104002362240268~3062604967
+// Banner Ad Unit ID = ca-app-pub-7104002362240268/6147965125
+// Interstitial Ad Unit ID = ca-app-pub-7104002362240268/5522915649
+// Rewarded Ad Unit ID = ca-app-pub-7104002362240268/1729357907
